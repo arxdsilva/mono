23ffe003a2594605c08340f64f5c80224dd0d436
Arthur Silva <arxdsilva@gmail.com>
2021-08-17 17:07:50 -0300

    add TestHelloByte

--- pkg1/lib_test.go
+++ pkg1/lib_test.go
@@ -1,6 +1,9 @@
 package pkg1
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestHelloString(t *testing.T) {
 	tests := []struct {
@@ -13,6 +16,22 @@ func TestHelloString(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := HelloString(); got != tt.want {
 				t.Errorf("HelloString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHelloByte(t *testing.T) {
+	tests := []struct {
+		name string
+		want []byte
+	}{
+		{name: "ok", want: []byte("Hello")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HelloByte(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("HelloByte() = %v, want %v", got, tt.want)
 			}
 		})
 	}
